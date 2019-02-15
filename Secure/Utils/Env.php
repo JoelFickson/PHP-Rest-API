979733991f9c043804c7b7967410bd19e87ef834
Joel F Ngozo <Joelngozo@gmail.com>
2019-02-14 21:18:54 -0800

    2019 Update

--- Secure/Utils/Env.php
+++ Secure/Utils/Env.php
@@ -13,4 +13,12 @@ class Env
     const  DB_NAME = "JoelRest";
     const  DB_SERVER = "localhost";
 
+    const APP_NAME = "Joel PHP REST";
+    const APP_HOME = "http://localhost:8008/Git";
+    const APP_HEADER = "http://localhost:8008/Git/Public/UI/Views/header";
+    const APP_FOOTER = "http://localhost:8008/Git/Public/UI/Views/footer";
+    const APP_ASSETS = "http://localhost:8008/Git/Public/UI/Assets/";
+
+
+
 }
